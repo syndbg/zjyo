6990a0d3aecac466ad55641371b4e9f7695738a9
Anton Antonov <anton.synd.antonov@gmail.com>
2026-09-19 01:22:29 +0300

    fix: add build.rs for git sha embedding
    Signed-off-by: Anton Antonov <anton.synd.antonov@gmail.com>

---
+++ build.rs
@@ -0,0 +1,16 @@
+use std::process::Command;
+
+fn main() {
+    let git_hash = Command::new("git")
+        .args(["rev-parse", "--short", "HEAD"])
+        .output()
+        .ok()
+        .filter(|output| output.status.success())
+        .and_then(|output| String::from_utf8(output.stdout).ok())
+        .map(|s| s.trim().to_string())
+        .unwrap_or_else(|| "unknown".to_string());
+
+    println!("cargo:rustc-env=ZJYO_GIT_HASH={git_hash}");
+    println!("cargo:rerun-if-changed=.git/HEAD");
+    println!("cargo:rerun-if-changed=.git/index");
+}
